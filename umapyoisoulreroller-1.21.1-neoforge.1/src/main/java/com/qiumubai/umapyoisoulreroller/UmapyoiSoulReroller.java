dd81c866d1ddd60527b40721b1ab4cc28fa01771
QiumuBai <aku150lty@gmail.com>
2026-04-15 22:45:41 -0700

    1

--- umapyoisoulreroller-1.21.1-neoforge.1/src/main/java/com/qiumubai/umapyoisoulreroller/UmapyoiSoulReroller.java
+++ umapyoisoulreroller-1.21.1-neoforge.1/src/main/java/com/qiumubai/umapyoisoulreroller/UmapyoiSoulReroller.java
@@ -5,7 +5,18 @@ import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -15,10 +26,10 @@ import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import com.qiumubai.umapyoisoulreroller.network.RerollFactorHandler;
-import com.qiumubai.umapyoisoulreroller.network.RerollFactorPayload;
-import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
-
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(UmapyoiSoulReroller.MODID)
@@ -33,7 +44,7 @@ public class UmapyoiSoulReroller {
     public UmapyoiSoulReroller(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-        modEventBus.addListener(this::registerPayloads);
+
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (UmapyoiSoulReroller) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
@@ -44,7 +55,6 @@ public class UmapyoiSoulReroller {
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-
     }
 
     private void commonSetup(FMLCommonSetupEvent event) {
@@ -61,19 +71,10 @@ public class UmapyoiSoulReroller {
 
     }
 
-    private void registerPayloads(final RegisterPayloadHandlersEvent event) {
-        event.registrar("1")
-                .playToServer(
-                        RerollFactorPayload.TYPE, // Ensure your payload has a static TYPE field
-                        RerollFactorPayload.STREAM_CODEC,
-                        RerollFactorHandler::handle
-                );
-    }
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
         // Do something when the server starts
         LOGGER.info("HELLO from server starting");
-
     }
 }
